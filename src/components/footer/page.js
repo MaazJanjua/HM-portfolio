b714f61b31d2aee5cf93fc7cbdc770689e02e3fa
hafiz maaz <hafizmaaz256@gmail.com>
2026-09-01 18:10:41 +0500

    Update footer heading style: change color to orange-500 and apply uppercase for better visibility

--- src/components/footer/page.js
+++ src/components/footer/page.js
@@ -165,7 +165,7 @@ const page = () => {
             </Link>
           </div> */}
           <div >
-            <h1 className="text-orange-600 lg:text-[1.1vw] text-[2.5vw]"> under-development</h1>
+            <h1 className="text-orange-500 lg:text-[1.1vw] text-[2.5vw] uppercase"> under-development</h1>
           </div>
           <div> <h2> site by Hafiz-Maaz</h2></div>
         </div>
